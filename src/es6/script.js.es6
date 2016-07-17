965ef7fb03685bb6544cfc6a7b5ec333da44da4e
Paul Murray <paul.murray@nytimes.com>
2016-07-17 15:20:30 -0400

    changes

--- src/es6/script.js.es6
+++ src/es6/script.js.es6
@@ -1,3 +1,6 @@
+// jshint esversion: 6, unused: true, undef: true
+/* global require, console, window */
+
 require([
   '_nytg/NYTG_SLUG/assets',
   '_nytg/NYTG_SLUG/big-assets',
@@ -6,35 +9,96 @@ require([
   'foundation/views/page-manager',
   'nyt5/analytics',
   'lib/text-balancer', // uncomment to balance headlines
-  'node_modules/d3/build/d3'
+  'node_modules/d3/build/d3',
+  '_nytg/data'
   // 'queue/1'
   // 'resizerScript'     // uncomment this line to include resizerScript
   // 'templates'         // uncomment to use src/templates
-], function(NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3) {
+], function(NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3, data) {
 
+  const margin = {top: 20, right: 20, bottom: 30, left: 40};
+  const height = 200;
   // begin code for your graphic here:
+  console.log(data);
 
-  d3.select('#g-graphic').append('h1').text('hello');
+  const svg = d3.select('#g-graphic')
+    .append('svg')
+    .attr('id', 'g-oly-1')
+    .classed('oly-chart', true)
+    .attr('height', height + margin.top + margin.bottom)
+    .style('border', '1px solid red');
+
+  const frame = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  const cluster = d3.cluster();
+
+  const x = d3.scaleTime()
+    .domain([new Date(2004, 0, 0), new Date(2015, 0, 0)]);
+
+  const root = get_hierarchy(data.rows);
+
+  cluster.size([ height, 0 ])(root);
+  const leaves = root.leaves();
+
+  const y = d3.scaleOrdinal()
+    .domain(leaves.map(d => d.data.value))
+    .range(leaves.map(d => d.x));
+
+  const medal_color = d3.scaleOrdinal()
+    .domain(['g','s','b'])
+    .range(['gold', 'silver', '#C9AE5D']);
+
+  frame.selectAll('.point')
+    .data(data.rows)
+    .enter()
+    .append('g')
+    .classed('point', true)
+    .each(function() {
+      // d3.select(this).append('text').text(d => d.medal)
+    })
+    .append('circle')
+    .attr('r', 3)
+    // .style('fill', d => medal_color(d.medal));
+    .style('fill', d => ['g','s','b'].indexOf(d.medal) !== -1 ? medal_color(d.medal) : 'red'); // medal_color(d.medal));
+    // .style('fill', d => { medal_color; debugger })
+
+  resizeAll();
+
+  function resizeAll() {
+    d3.selectAll('.oly-chart')
+      .attr('width', function() {
+        return $(this.parentNode).innerWidth();
+      });
+    var newWidth = svg.node().parentNode.clientWidth - margin.left - margin.right;
+    x.rangeRound([0, newWidth]);
+    frame.selectAll('.point')
+      .attr('transform', ({ year, month, day, full_name }) =>  {
+        const date = new Date(year, parseInt(month) - 1, day);
+        return `translate(${x(date)}, ${y(full_name)})`;
+      });
+  }
+
+  // function setPositions() {
+  //
+  // }
+  //
+  // function updateY({ width, height, y }) {
+  //
+  // }
+
+  d3.select(window).on('resize', resizeAll);
 
 
 
-
-
-
-  // uncomment to balance headline and leadin
-  // balanceText('.interactive-headline, .interactive-leadin');
-
-  // templates
-  // var html = Templates.jst.example_template({ text: "yo" });
-
-  // custom sharetools
-  // <div class="sharetools g-sharetools" data-url="http://www.nytimes.com" data-title="Custom Title"></div>
-  // require(['interactive/main'], function() {
-  //   require(['shared/sharetools/views/share-tools-container'], function(ShareTools) {
-  //     $(".g-sharetools").each(function() {
-  //       new ShareTools({ el: $(this) });
-  //     });
-  //   });
-  // });
-
+  function get_hierarchy(data) {
+    const nested = d3.nest()
+      .key(() => "_root")
+      .key(d => d.id)
+      .key(d => d.athlete_order)
+      .rollup(d => d[0].full_name)
+      .entries(data);
+    const tree = d3.hierarchy(nested[0], d => d.values);
+    return tree;
+  }
 }); // end require
--- src/script.js
+++ src/script.js
@@ -1,29 +1,92 @@
 'use strict';
 
+// jshint esversion: 6, unused: true, undef: true
+/* global require, console, window */
+
 require(['_nytg/NYTG_SLUG/assets', '_nytg/NYTG_SLUG/big-assets', 'jquery/nyt', 'underscore/1.6', 'foundation/views/page-manager', 'nyt5/analytics', 'lib/text-balancer', // uncomment to balance headlines
-'node_modules/d3/build/d3'
+'node_modules/d3/build/d3', '_nytg/data'
 // 'queue/1'
 // 'resizerScript'     // uncomment this line to include resizerScript
 // 'templates'         // uncomment to use src/templates
-], function (NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3) {
+], function (NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3, data) {
 
+  var margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  var height = 200;
   // begin code for your graphic here:
+  console.log(data);
 
-  d3.select('#g-graphic').append('h1').text('hello');
+  var svg = d3.select('#g-graphic').append('svg').attr('id', 'g-oly-1').classed('oly-chart', true).attr('height', height + margin.top + margin.bottom).style('border', '1px solid red');
 
-  // uncomment to balance headline and leadin
-  // balanceText('.interactive-headline, .interactive-leadin');
+  var frame = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-  // templates
-  // var html = Templates.jst.example_template({ text: "yo" });
+  var cluster = d3.cluster();
 
-  // custom sharetools
-  // <div class="sharetools g-sharetools" data-url="http://www.nytimes.com" data-title="Custom Title"></div>
-  // require(['interactive/main'], function() {
-  //   require(['shared/sharetools/views/share-tools-container'], function(ShareTools) {
-  //     $(".g-sharetools").each(function() {
-  //       new ShareTools({ el: $(this) });
-  //     });
-  //   });
-  // });
+  var x = d3.scaleTime().domain([new Date(2004, 0, 0), new Date(2015, 0, 0)]);
+
+  var root = get_hierarchy(data.rows);
+
+  cluster.size([height, 0])(root);
+  var leaves = root.leaves();
+
+  var y = d3.scaleOrdinal().domain(leaves.map(function (d) {
+    return d.data.value;
+  })).range(leaves.map(function (d) {
+    return d.x;
+  }));
+
+  var medal_color = d3.scaleOrdinal().domain(['g', 's', 'b']).range(['gold', 'silver', '#C9AE5D']);
+
+  frame.selectAll('.point').data(data.rows).enter().append('g').classed('point', true).each(function () {
+    // d3.select(this).append('text').text(d => d.medal)
+  }).append('circle').attr('r', 3)
+  // .style('fill', d => medal_color(d.medal));
+  .style('fill', function (d) {
+    return ['g', 's', 'b'].indexOf(d.medal) !== -1 ? medal_color(d.medal) : 'red';
+  }); // medal_color(d.medal));
+  // .style('fill', d => { medal_color; debugger })
+
+  resizeAll();
+
+  function resizeAll() {
+    d3.selectAll('.oly-chart').attr('width', function () {
+      return $(this.parentNode).innerWidth();
+    });
+    var newWidth = svg.node().parentNode.clientWidth - margin.left - margin.right;
+    x.rangeRound([0, newWidth]);
+    frame.selectAll('.point').attr('transform', function (_ref) {
+      var year = _ref.year;
+      var month = _ref.month;
+      var day = _ref.day;
+      var full_name = _ref.full_name;
+
+      var date = new Date(year, parseInt(month) - 1, day);
+      return 'translate(' + x(date) + ', ' + y(full_name) + ')';
+    });
+  }
+
+  // function setPositions() {
+  //
+  // }
+  //
+  // function updateY({ width, height, y }) {
+  //
+  // }
+
+  d3.select(window).on('resize', resizeAll);
+
+  function get_hierarchy(data) {
+    var nested = d3.nest().key(function () {
+      return "_root";
+    }).key(function (d) {
+      return d.id;
+    }).key(function (d) {
+      return d.athlete_order;
+    }).rollup(function (d) {
+      return d[0].full_name;
+    }).entries(data);
+    var tree = d3.hierarchy(nested[0], function (d) {
+      return d.values;
+    });
+    return tree;
+  }
 }); // end require
