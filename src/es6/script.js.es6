f8f43165bc212c8fca2363236068239c8b58af0c
Paul Murray <paul.murray@nytimes.com>
2016-07-25 18:53:25 -0400

    v2

--- src/es6/script.js.es6
+++ src/es6/script.js.es6
@@ -1,5 +1,5 @@
 // jshint esversion: 6, unused: true, undef: true
-/* global require, console, window */
+/* global require, console, window */ // jshint ignore:line
 
 require([
   '_nytg/NYTG_SLUG/assets',
@@ -10,95 +10,221 @@ require([
   'nyt5/analytics',
   'lib/text-balancer', // uncomment to balance headlines
   'node_modules/d3/build/d3',
-  '_nytg/data'
+  // 'node_modules/d3-jetpack/d3-jetpack',
+  '_nytg/data',
+  'node_modules/rx/dist/rx.all'
   // 'queue/1'
   // 'resizerScript'     // uncomment this line to include resizerScript
   // 'templates'         // uncomment to use src/templates
-], function(NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3, data) {
-
-  const margin = {top: 20, right: 20, bottom: 30, left: 40};
-  const height = 200;
-  // begin code for your graphic here:
-  console.log(data);
-
-  const svg = d3.select('#g-graphic')
-    .append('svg')
-    .attr('id', 'g-oly-1')
-    .classed('oly-chart', true)
-    .attr('height', height + margin.top + margin.bottom)
-    .style('border', '1px solid red');
-
-  const frame = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-  const cluster = d3.cluster();
-
-  const x = d3.scaleTime()
-    .domain([new Date(2004, 0, 0), new Date(2015, 0, 0)]);
-
-  const root = get_hierarchy(data.rows);
-
-  cluster.size([ height, 0 ])(root);
-  const leaves = root.leaves();
-
-  const y = d3.scaleOrdinal()
-    .domain(leaves.map(d => d.data.value))
-    .range(leaves.map(d => d.x));
-
-  const medal_color = d3.scaleOrdinal()
-    .domain(['g','s','b'])
-    .range(['gold', 'silver', '#C9AE5D']);
-
-  frame.selectAll('.point')
-    .data(data.rows)
-    .enter()
-    .append('g')
-    .classed('point', true)
-    .each(function() {
-      // d3.select(this).append('text').text(d => d.medal)
-    })
-    .append('circle')
-    .attr('r', 3)
-    // .style('fill', d => medal_color(d.medal));
-    .style('fill', d => ['g','s','b'].indexOf(d.medal) !== -1 ? medal_color(d.medal) : 'red'); // medal_color(d.medal));
-    // .style('fill', d => { medal_color; debugger })
-
-  resizeAll();
-
-  function resizeAll() {
-    d3.selectAll('.oly-chart')
-      .attr('width', function() {
-        return $(this.parentNode).innerWidth();
-      });
-    var newWidth = svg.node().parentNode.clientWidth - margin.left - margin.right;
-    x.rangeRound([0, newWidth]);
-    frame.selectAll('.point')
-      .attr('transform', ({ year, month, day, full_name }) =>  {
-        const date = new Date(year, parseInt(month) - 1, day);
-        return `translate(${x(date)}, ${y(full_name)})`;
-      });
-  }
-
-  // function setPositions() {
-  //
-  // }
-  //
-  // function updateY({ width, height, y }) {
-  //
-  // }
-
-  d3.select(window).on('resize', resizeAll);
-
-
-
-  function get_hierarchy(data) {
-    const nested = d3.nest()
-      .key(() => "_root")
-      .key(d => d.id)
-      .key(d => d.athlete_order)
-      .rollup(d => d[0].full_name)
-      .entries(data);
-    const tree = d3.hierarchy(nested[0], d => d.values);
-    return tree;
-  }
+], function(NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3, data, Rx) { // jshint ignore:line
+  // version_1(d3, $)(data);
+  // require(['node_modules/d3-jetpack/d3-jetpack'], function(_d3) { debugger });
+  version_2(d3, $, Rx)(d3.select('#g-graphic'), data.rows_v2);
+  // console.log(data.rows_v2);
 }); // end require
+
+function version_2(d3, $, Rx) {
+  const stream = Rx.Observable;
+  return function(container, data) {
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+    const height = 200;
+
+    data.sort((a,b) => d3.ascending(parseInt(a.oly_year), parseInt(b.oly_year)));
+    data.shift();
+    const years = data.map(d => parseInt(d.oly_year))
+      .concat(data.map(d => (new Date(d.decision_date).getFullYear())));
+    const year_extent = d3.extent(years);
+    const x_domain = [new Date(year_extent[0], 0, 1), new Date(year_extent[1] + 1, 0, 1)];
+
+    const x = d3.scaleTime()
+      .domain(x_domain);
+
+    const x_axis = d3.axisBottom(x)
+      .tickSize(-height)
+      .tickSizeOuter(0);
+
+    const y = d3.scalePoint()
+      .domain(data.map((d,i) => i))
+      .range([0, height])
+      .padding(0.5);
+
+    const svg = container
+      .append('svg')
+      .attr('id', 'g-oly-1')
+      .classed('oly-chart', true)
+      .attr('height', height + margin.top + margin.bottom)
+      .style('border', '1px solid #ddd');
+
+    const frame = svg.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    frame.append('g')
+      .classed('axis axis--x', true)
+      .attr('transform', `translate(0, ${height})`);
+
+    const rows_join = frame.selectAll('g.row')
+      .data(data);
+
+    const rows = rows_join.enter()
+      .append('g')
+      .classed('row', true)
+      .each(function(d) {
+        d3.select(this)
+          .append('g').classed('line', true)
+          .append('line')
+          .style('stroke', '#555');
+      })
+      .merge(rows_join)
+      .attr('transform', (d, i) => `translate(0, ${y(i)})`);
+
+    const MONTH = 8 - 1; // August
+    const DAY = 12;
+
+    function getPoints(datum) {
+      return [
+        {
+          type: 'awarded',
+          datum,
+          parsed_date: new Date(parseInt(datum.oly_year), MONTH, DAY)
+        },
+        {
+          type: 'stripped',
+          datum,
+          parsed_date: new Date(datum.decision_date)
+        }
+      ];
+    }
+
+    const medal_color = d3.scaleOrdinal()
+      .domain(['g','s','b'])
+      .range(['gold', 'silver', '#C9AE5D']);
+
+    const points_join = rows.selectAll('g.point')
+      .data(getPoints);
+
+    const points = points_join.enter()
+      .append('g')
+      .classed('point', true)
+      .each(function(d) {
+        if (d.type === 'awarded') {
+          d3.select(this).append('circle')
+            .attr('r', 4)
+            .style('fill', d => medal_color(d.datum.medal));
+          d3.select(this).append('text')
+            .style('font-family', 'nyt-franklin')
+            .style('font-size', '0.7rem')
+            .style('fill', '#555')
+            .attr('dy', '-0.2rem')
+            .attr('dx', '0.3rem')
+            .text(({ datum }) => `${datum.ath_first_name} ${datum.ath_last_name}`.toUpperCase());
+        }
+
+      })
+      .merge(points_join);
+
+    // rows.selectAll('g.point')
+
+    const width$ = stream.fromEvent(window, 'resize')
+      .startWith(window)
+      .map(() => $(svg.node().parentNode).innerWidth());
+
+    width$.subscribe(chartWidth => {
+      console.log(chartWidth);
+      x_axis.ticks(d3.timeYear.every(2))
+      const width = chartWidth - margin.left - margin.right;
+      x.range([0, width]);
+      container.selectAll('.oly-chart')
+        .attr('width', chartWidth)
+        .select('.axis--x')
+        .call(x_axis)
+        .each(function() {
+          d3.select(this).selectAll('line').style('stroke', '#ddd');
+          d3.select(this).selectAll('text').style('font-family', '"nyt-franklin"');
+        });
+      points.attr('transform', d => `translate(${x(d.parsed_date)}, 0)`);
+      rows.select('line')
+        .datum(d => getPoints(d).map(d => d.parsed_date))
+        // .attr('x0', d => { x; debugger })
+        .attr('x1', d => x(d[0]))
+        .attr('x2', d => x(d[1]));
+    });
+  };
+}
+
+function version_1(d3, $) { // jshint ignore:line
+  return function(data) {
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+    const height = 200;
+
+    const svg = d3.select('#g-graphic')
+      .append('svg')
+      .attr('id', 'g-oly-1')
+      .classed('oly-chart', true)
+      .attr('height', height + margin.top + margin.bottom)
+      .style('border', '1px solid red');
+
+    const frame = svg.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    const cluster = d3.cluster();
+
+    const x = d3.scaleTime()
+      .domain([new Date(2004, 0, 0), new Date(2015, 0, 0)]);
+
+    const root = get_hierarchy(data.rows);
+
+    cluster.size([ height, 0 ])(root);
+    const leaves = root.leaves();
+
+    const y = d3.scaleOrdinal()
+      .domain(leaves.map(d => d.data.value))
+      .range(leaves.map(d => d.x));
+
+    const medal_color = d3.scaleOrdinal()
+      .domain(['g','s','b'])
+      .range(['gold', 'silver', '#C9AE5D']);
+
+    frame.selectAll('.point')
+      .data(data.rows)
+      .enter()
+      .append('g')
+      .classed('point', true)
+      .each(function() {
+        // d3.select(this).append('text').text(d => d.medal)
+      })
+      .append('circle')
+      .attr('r', 3)
+      // .style('fill', d => medal_color(d.medal));
+      .style('fill', d => ['g','s','b'].indexOf(d.medal) !== -1 ? medal_color(d.medal) : 'red'); // medal_color(d.medal));
+      // .style('fill', d => { medal_color; debugger })
+
+    resizeAll();
+
+    function resizeAll() {
+      d3.selectAll('.oly-chart')
+        .attr('width', function() {
+          return $(this.parentNode).innerWidth();
+        });
+      var newWidth = svg.node().parentNode.clientWidth - margin.left - margin.right;
+      x.rangeRound([0, newWidth]);
+      frame.selectAll('.point')
+        .attr('transform', ({ year, month, day, full_name }) =>  {
+          const date = new Date(year, parseInt(month) - 1, day);
+          return `translate(${x(date)}, ${y(full_name)})`;
+        });
+    }
+
+    d3.select(window).on('resize', resizeAll);
+
+    function get_hierarchy(data) {
+      const nested = d3.nest()
+        .key(() => "_root")
+        .key(d => d.id)
+        .key(d => d.athlete_order)
+        .rollup(d => d[0].full_name)
+        .entries(data);
+      const tree = d3.hierarchy(nested[0], d => d.values);
+      return tree;
+    }
+  };
+}
--- src/script.js
+++ src/script.js
@@ -1,92 +1,205 @@
 'use strict';
 
 // jshint esversion: 6, unused: true, undef: true
-/* global require, console, window */
+/* global require, console, window */ // jshint ignore:line
 
 require(['_nytg/NYTG_SLUG/assets', '_nytg/NYTG_SLUG/big-assets', 'jquery/nyt', 'underscore/1.6', 'foundation/views/page-manager', 'nyt5/analytics', 'lib/text-balancer', // uncomment to balance headlines
-'node_modules/d3/build/d3', '_nytg/data'
+'node_modules/d3/build/d3',
+// 'node_modules/d3-jetpack/d3-jetpack',
+'_nytg/data', 'node_modules/rx/dist/rx.all'
 // 'queue/1'
 // 'resizerScript'     // uncomment this line to include resizerScript
 // 'templates'         // uncomment to use src/templates
-], function (NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3, data) {
-
-  var margin = { top: 20, right: 20, bottom: 30, left: 40 };
-  var height = 200;
-  // begin code for your graphic here:
-  console.log(data);
-
-  var svg = d3.select('#g-graphic').append('svg').attr('id', 'g-oly-1').classed('oly-chart', true).attr('height', height + margin.top + margin.bottom).style('border', '1px solid red');
-
-  var frame = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
-  var cluster = d3.cluster();
-
-  var x = d3.scaleTime().domain([new Date(2004, 0, 0), new Date(2015, 0, 0)]);
-
-  var root = get_hierarchy(data.rows);
-
-  cluster.size([height, 0])(root);
-  var leaves = root.leaves();
-
-  var y = d3.scaleOrdinal().domain(leaves.map(function (d) {
-    return d.data.value;
-  })).range(leaves.map(function (d) {
-    return d.x;
-  }));
-
-  var medal_color = d3.scaleOrdinal().domain(['g', 's', 'b']).range(['gold', 'silver', '#C9AE5D']);
-
-  frame.selectAll('.point').data(data.rows).enter().append('g').classed('point', true).each(function () {
-    // d3.select(this).append('text').text(d => d.medal)
-  }).append('circle').attr('r', 3)
-  // .style('fill', d => medal_color(d.medal));
-  .style('fill', function (d) {
-    return ['g', 's', 'b'].indexOf(d.medal) !== -1 ? medal_color(d.medal) : 'red';
-  }); // medal_color(d.medal));
-  // .style('fill', d => { medal_color; debugger })
-
-  resizeAll();
-
-  function resizeAll() {
-    d3.selectAll('.oly-chart').attr('width', function () {
-      return $(this.parentNode).innerWidth();
-    });
-    var newWidth = svg.node().parentNode.clientWidth - margin.left - margin.right;
-    x.rangeRound([0, newWidth]);
-    frame.selectAll('.point').attr('transform', function (_ref) {
-      var year = _ref.year;
-      var month = _ref.month;
-      var day = _ref.day;
-      var full_name = _ref.full_name;
-
-      var date = new Date(year, parseInt(month) - 1, day);
-      return 'translate(' + x(date) + ', ' + y(full_name) + ')';
-    });
-  }
-
-  // function setPositions() {
-  //
-  // }
-  //
-  // function updateY({ width, height, y }) {
-  //
-  // }
-
-  d3.select(window).on('resize', resizeAll);
-
-  function get_hierarchy(data) {
-    var nested = d3.nest().key(function () {
-      return "_root";
-    }).key(function (d) {
-      return d.id;
-    }).key(function (d) {
-      return d.athlete_order;
-    }).rollup(function (d) {
-      return d[0].full_name;
-    }).entries(data);
-    var tree = d3.hierarchy(nested[0], function (d) {
-      return d.values;
-    });
-    return tree;
-  }
+], function (NYTG_ASSETS, NYTG_BIG_ASSETS, $, _, PageManager, Analytics, balanceText, d3, data, Rx) {
+  // jshint ignore:line
+  // version_1(d3, $)(data);
+  // require(['node_modules/d3-jetpack/d3-jetpack'], function(_d3) { debugger });
+  version_2(d3, $, Rx)(d3.select('#g-graphic'), data.rows_v2);
+  // console.log(data.rows_v2);
 }); // end require
+
+function version_2(d3, $, Rx) {
+  var stream = Rx.Observable;
+  return function (container, data) {
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    var height = 200;
+
+    data.sort(function (a, b) {
+      return d3.ascending(parseInt(a.oly_year), parseInt(b.oly_year));
+    });
+    data.shift();
+    var years = data.map(function (d) {
+      return parseInt(d.oly_year);
+    }).concat(data.map(function (d) {
+      return new Date(d.decision_date).getFullYear();
+    }));
+    var year_extent = d3.extent(years);
+    var x_domain = [new Date(year_extent[0], 0, 1), new Date(year_extent[1] + 1, 0, 1)];
+
+    var x = d3.scaleTime().domain(x_domain);
+
+    var x_axis = d3.axisBottom(x).tickSize(-height).tickSizeOuter(0);
+
+    var y = d3.scalePoint().domain(data.map(function (d, i) {
+      return i;
+    })).range([0, height]).padding(0.5);
+
+    var svg = container.append('svg').attr('id', 'g-oly-1').classed('oly-chart', true).attr('height', height + margin.top + margin.bottom).style('border', '1px solid #ddd');
+
+    var frame = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+    frame.append('g').classed('axis axis--x', true).attr('transform', 'translate(0, ' + height + ')');
+
+    var rows_join = frame.selectAll('g.row').data(data);
+
+    var rows = rows_join.enter().append('g').classed('row', true)
+    // .each(function() { d3.select(this).append('circle').attr('r', 2); })
+    .each(function (d) {
+      d3.select(this).append('g').classed('line', true).append('line').style('stroke', '#555');
+      // .datum(123);
+      // .datum(d => getPoints(d).map(d => d.parsed_date));
+    }).merge(rows_join).attr('transform', function (d, i) {
+      return 'translate(0, ' + y(i) + ')';
+    });
+
+    // const line_join = rows.selectAll('g.line')
+    //   .data(d => [getPoints(d)])
+
+    var MONTH = 8 - 1; // August
+    var DAY = 12;
+
+    function getPoints(datum) {
+      return [{
+        type: 'awarded',
+        datum: datum,
+        parsed_date: new Date(parseInt(datum.oly_year), MONTH, DAY)
+      }, {
+        type: 'stripped',
+        datum: datum,
+        parsed_date: new Date(datum.decision_date)
+      }];
+    }
+
+    var medal_color = d3.scaleOrdinal().domain(['g', 's', 'b']).range(['gold', 'silver', '#C9AE5D']);
+
+    var points_join = rows.selectAll('g.point').data(getPoints);
+
+    var points = points_join.enter().append('g').classed('point', true).each(function (d) {
+      if (d.type === 'awarded') {
+        d3.select(this).append('circle').attr('r', 4).style('fill', function (d) {
+          return medal_color(d.datum.medal);
+        });
+        d3.select(this).append('text').style('font-family', 'nyt-franklin').style('font-size', '0.7rem').style('fill', '#555').attr('dy', '-0.2rem').attr('dx', '0.3rem').text(function (_ref) {
+          var datum = _ref.datum;
+          return (datum.ath_first_name + ' ' + datum.ath_last_name).toUpperCase();
+        });
+      }
+    }).merge(points_join);
+
+    // rows.selectAll('g.point')
+
+    var width$ = stream.fromEvent(window, 'resize').startWith(window).map(function () {
+      return $(svg.node().parentNode).innerWidth();
+    });
+
+    width$.subscribe(function (chartWidth) {
+      console.log(chartWidth);
+      x_axis.ticks(d3.timeYear.every(2));
+      var width = chartWidth - margin.left - margin.right;
+      x.range([0, width]);
+      container.selectAll('.oly-chart').attr('width', chartWidth).select('.axis--x').call(x_axis).each(function () {
+        d3.select(this).selectAll('line').style('stroke', '#ddd');
+        d3.select(this).selectAll('text').style('font-family', '"nyt-franklin"');
+      });
+      points.attr('transform', function (d) {
+        return 'translate(' + x(d.parsed_date) + ', 0)';
+      });
+      rows.select('line').datum(function (d) {
+        return getPoints(d).map(function (d) {
+          return d.parsed_date;
+        });
+      })
+      // .attr('x0', d => { x; debugger })
+      .attr('x1', function (d) {
+        return x(d[0]);
+      }).attr('x2', function (d) {
+        return x(d[1]);
+      });
+    });
+  };
+}
+
+function version_1(d3, $) {
+  // jshint ignore:line
+  return function (data) {
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    var height = 200;
+
+    var svg = d3.select('#g-graphic').append('svg').attr('id', 'g-oly-1').classed('oly-chart', true).attr('height', height + margin.top + margin.bottom).style('border', '1px solid red');
+
+    var frame = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+    var cluster = d3.cluster();
+
+    var x = d3.scaleTime().domain([new Date(2004, 0, 0), new Date(2015, 0, 0)]);
+
+    var root = get_hierarchy(data.rows);
+
+    cluster.size([height, 0])(root);
+    var leaves = root.leaves();
+
+    var y = d3.scaleOrdinal().domain(leaves.map(function (d) {
+      return d.data.value;
+    })).range(leaves.map(function (d) {
+      return d.x;
+    }));
+
+    var medal_color = d3.scaleOrdinal().domain(['g', 's', 'b']).range(['gold', 'silver', '#C9AE5D']);
+
+    frame.selectAll('.point').data(data.rows).enter().append('g').classed('point', true).each(function () {
+      // d3.select(this).append('text').text(d => d.medal)
+    }).append('circle').attr('r', 3)
+    // .style('fill', d => medal_color(d.medal));
+    .style('fill', function (d) {
+      return ['g', 's', 'b'].indexOf(d.medal) !== -1 ? medal_color(d.medal) : 'red';
+    }); // medal_color(d.medal));
+    // .style('fill', d => { medal_color; debugger })
+
+    resizeAll();
+
+    function resizeAll() {
+      d3.selectAll('.oly-chart').attr('width', function () {
+        return $(this.parentNode).innerWidth();
+      });
+      var newWidth = svg.node().parentNode.clientWidth - margin.left - margin.right;
+      x.rangeRound([0, newWidth]);
+      frame.selectAll('.point').attr('transform', function (_ref2) {
+        var year = _ref2.year;
+        var month = _ref2.month;
+        var day = _ref2.day;
+        var full_name = _ref2.full_name;
+
+        var date = new Date(year, parseInt(month) - 1, day);
+        return 'translate(' + x(date) + ', ' + y(full_name) + ')';
+      });
+    }
+
+    d3.select(window).on('resize', resizeAll);
+
+    function get_hierarchy(data) {
+      var nested = d3.nest().key(function () {
+        return "_root";
+      }).key(function (d) {
+        return d.id;
+      }).key(function (d) {
+        return d.athlete_order;
+      }).rollup(function (d) {
+        return d[0].full_name;
+      }).entries(data);
+      var tree = d3.hierarchy(nested[0], function (d) {
+        return d.values;
+      });
+      return tree;
+    }
+  };
+}
